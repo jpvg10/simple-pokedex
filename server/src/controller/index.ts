062a46d0d1ff0be127e672ac94700ec212a2efd7
jpvg10 <juan.valencia10a@gmail.com>
2020-05-02 19:15:26 -0500

    Add remaining endpoints

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -16,9 +16,16 @@ export const getPokedexes: RequestHandler = async (req, res) => {
   }
 };
 
-export const getPokedex: RequestHandler = async (req, res) => {
+export const getPokemonFromPokedex: RequestHandler = async (req, res) => {
   try {
-    const pokedexName = req.params.name;
+    const pokedexName = req.query.pokedex;
+
+    if (!pokedexName) {
+      res.status(400).send({
+        message: 'Provide query parameter "pokedex"'
+      });
+    }
+
     const result = await axios.get(`https://pokeapi.co/api/v2/pokedex/${pokedexName}/`);
 
     const pokedex = result.data;
@@ -26,6 +33,109 @@ export const getPokedex: RequestHandler = async (req, res) => {
       return {
         id: pokemon.entry_number,
         name: pokemon.pokemon_species.name
+      };
+    });
+
+    res.status(200).send(arrayPokemon);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+
+export const getPokemon: RequestHandler = async (req, res) => {
+  try {
+    const pokemonName = req.params.name;
+    const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+    const pokemon = result.data;
+
+    const arrayTypes = pokemon.types.map((type: any) => type.type.name);
+
+    const arrayStats = pokemon.stats.map((stat: any) => {
+      return {
+        name: stat.stat.name,
+        baseStat: stat.base_stat
+      };
+    });
+
+    const abilityPromises = pokemon.abilities.map((ability: any) => axios.get(ability.ability.url));
+    const abilityResponses = await Promise.all(abilityPromises);
+    const arrayAbilities = abilityResponses.map((response: any) => {
+      return {
+        name: response.data.name,
+        effect: response.data.effect_entries[0].effect
+      };
+    });
+
+    const typePromises = pokemon.types.map((type: any) => axios.get(type.type.url));
+    const typeResponses = await Promise.all(typePromises);
+
+    const defensesMap = new Map();
+    for (const response of typeResponses) {
+      const damageRelations = (response as any).data.damage_relations;
+      damageRelations.no_damage_from.forEach((element: any) => {
+        defensesMap.set(element.name, 0);
+      });
+      damageRelations.half_damage_from.forEach((element: any) => {
+        if (defensesMap.has(element.name)) {
+          const def = defensesMap.get(element.name);
+          defensesMap.set(element.name, def * 0.5);
+        } else {
+          defensesMap.set(element.name, 0.5);
+        }
+      });
+      damageRelations.double_damage_from.forEach((element: any) => {
+        if (defensesMap.has(element.name)) {
+          const def = defensesMap.get(element.name);
+          defensesMap.set(element.name, def * 2);
+        } else {
+          defensesMap.set(element.name, 2);
+        }
+      });
+    }
+
+    const arrayDefenses: any[] = [];
+    defensesMap.forEach((value, key) => {
+      if (value !== 1) {
+        arrayDefenses.push({ type: key, value });
+      }
+    });
+
+    const pokemonData = {
+      name: pokemon.name,
+      number: pokemon.id,
+      front: pokemon.sprites.front_default,
+      back: pokemon.sprites.back_default,
+      types: arrayTypes,
+      stats: arrayStats,
+      abilities: arrayAbilities,
+      defenses: arrayDefenses
+    };
+
+    res.status(200).send(pokemonData);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+
+export const getRandomTeam: RequestHandler = async (req, res) => {
+  try {
+    const pokedexName = req.params.pokedex;
+    const pokedexResult = await axios.get(`https://pokeapi.co/api/v2/pokedex/${pokedexName}/`);
+    const pokedex = pokedexResult.data;
+
+    const count = pokedex.pokemon_entries.length;
+    const pokemonPromises: Promise<any>[] = [];
+    for (let i = 0; i < 6; i++) {
+      const chosen = Math.floor(Math.random() * count);
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokedex.pokemon_entries[chosen].pokemon_species.name}/`;
+      pokemonPromises.push(axios.get(url));
+    }
+
+    const pokemonResponses = await Promise.all(pokemonPromises);
+    const arrayPokemon = pokemonResponses.map((response) => {
+      return {
+        name: response.data.name,
+        front: response.data.sprites.front_default
       };
     });
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,22 +1,14 @@
 import express, { Request, Response } from 'express';
-import { getPokedexes, getPokedex } from '../controller';
+import { getPokedexes, getPokemonFromPokedex, getPokemon, getRandomTeam } from '../controller';
 
 const router = express.Router();
 
 router.get('/pokedex/', getPokedexes);
 
-router.get('/pokedex/:name', getPokedex);
+router.get('/pokemon/', getPokemonFromPokedex);
 
-router.get('/pokemon/:name', (req: Request, res: Response) => {
-  res.json({
-    message: 'pokemon'
-  });
-});
+router.get('/pokemon/:name', getPokemon);
 
-router.get('/random_team/:pokedex_name', (req: Request, res: Response) => {
-  res.json({
-    message: 'team'
-  });
-});
+router.get('/random_team/:pokedex', getRandomTeam);
 
 export default router;
