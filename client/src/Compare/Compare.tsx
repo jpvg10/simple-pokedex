a00002650977a4e94bd2ed227460302c7193846f
jpvg10 <juan.valencia10a@gmail.com>
2020-05-12 20:27:39 -0500

    Compare selects

--- client/src/Compare/Compare.tsx
+++ client/src/Compare/Compare.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Typography, FormControl, InputLabel, Select, MenuItem, makeStyles, Grid, Button } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Typography, FormControl, InputLabel, makeStyles, Grid, Button } from '@material-ui/core';
+import { getPokedexDetail } from '../utils/api';
+import { IPokemonBasic } from '../utils/interfaces';
+import Select from 'react-select';
 
 const useStyles = makeStyles(() => ({
   select: {
@@ -7,17 +10,40 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
+interface SelectOptions {
+  value: string;
+  label: string;
+}
+
 const Compare: React.FC = () => {
   const classes = useStyles();
 
-  const pokemonList = ['Pikachu', 'Charmander'];
+  const [pokemonList, setPokemonList] = useState<SelectOptions[]>([]);
+  const [selected, setSelected] = useState<SelectOptions[]>([]);
 
-  const [selected, setSelected] = React.useState([0, 0]);
+  useEffect(() => {
+    const loadPokemonList = async () => {
+      const detail = await getPokedexDetail('national');
+      if (detail) {
+        const list: SelectOptions[] = detail.pokemon.map((poke: IPokemonBasic) => ({
+          value: poke.id,
+          label: poke.name
+        }));
+        setPokemonList(list);
+        setSelected([list[0], list[0]]);
+      }
+    };
+    loadPokemonList();
+  }, []);
 
-  const handler = (index: number) => (event: React.ChangeEvent<{ value: unknown }>): void => {
+  const handler = (index: number) => (selectedOption: any): void => {
     const newSelected = [...selected];
-    newSelected[index] = event.target.value as number;
+    newSelected[index] = selectedOption;
     setSelected(newSelected);
+  };
+
+  const onClickGo = async () => {
+    console.log(selected);
   };
 
   return (
@@ -27,39 +53,17 @@ const Compare: React.FC = () => {
         <Grid item xs={12} md={5}>
           <FormControl className={classes.select}>
             <InputLabel id="demo-simple-select-label">Choose a Pokémon</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={selected[0]}
-              onChange={handler(0)}
-            >
-              {pokemonList.map((name, index) => (
-                <MenuItem value={index} key={index}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
+            <Select options={pokemonList} value={selected[0]} onChange={handler(0)} className="capitalize" />
           </FormControl>
         </Grid>
         <Grid item xs={12} md={5}>
           <FormControl className={classes.select}>
             <InputLabel id="demo-simple-select-label">Choose a Pokémon</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={selected[1]}
-              onChange={handler(1)}
-            >
-              {pokemonList.map((name, index) => (
-                <MenuItem value={index} key={index}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
+            <Select options={pokemonList} value={selected[1]} onChange={handler(1)} className="capitalize" />
           </FormControl>
         </Grid>
         <Grid item xs={2}>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={onClickGo}>
             Go!
           </Button>
         </Grid>
