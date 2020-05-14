6cbe5cee66852a61fc6c2aea96200ec4a21a007d
jpvg10 <juan.valencia10a@gmail.com>
2020-05-13 21:38:38 -0500

    Display pokemon on the compare page

--- client/src/Compare/Compare.tsx
+++ client/src/Compare/Compare.tsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, FormControl, InputLabel, makeStyles, Grid, Button } from '@material-ui/core';
-import { getPokedexDetail } from '../utils/api';
-import { IPokemonBasic } from '../utils/interfaces';
+import React, { useState, useEffect, useRef } from 'react';
 import Select from 'react-select';
+import { Typography, makeStyles, Grid, Button } from '@material-ui/core';
+import { getPokedexDetail, getPokemonDetail } from '../utils/api';
+import { IPokemonBasic, IPokemonDetail } from '../utils/interfaces';
+import PokemonDetail from '../PokemonDetail';
 
 const useStyles = makeStyles(() => ({
   select: {
-    minWidth: '100%'
+    textTransform: 'capitalize',
+    fontFamily: 'Roboto, Helvetica, Arial, "sans-serif"'
   }
 }));
 
-interface SelectOptions {
+interface ISelectOptions {
   value: string;
   label: string;
 }
@@ -18,14 +20,13 @@ interface SelectOptions {
 const Compare: React.FC = () => {
   const classes = useStyles();
 
-  const [pokemonList, setPokemonList] = useState<SelectOptions[]>([]);
-  const [selected, setSelected] = useState<SelectOptions[]>([]);
+  const [pokemonList, setPokemonList] = useState<ISelectOptions[]>([]);
 
   useEffect(() => {
     const loadPokemonList = async () => {
       const detail = await getPokedexDetail('national');
       if (detail) {
-        const list: SelectOptions[] = detail.pokemon.map((poke: IPokemonBasic) => ({
+        const list: ISelectOptions[] = detail.pokemon.map((poke: IPokemonBasic) => ({
           value: poke.id,
           label: poke.name
         }));
@@ -36,14 +37,30 @@ const Compare: React.FC = () => {
     loadPokemonList();
   }, []);
 
+  const [selected, setSelected] = useState<ISelectOptions[]>([]);
+
   const handler = (index: number) => (selectedOption: any): void => {
     const newSelected = [...selected];
-    newSelected[index] = selectedOption;
+    newSelected[index] = selectedOption as ISelectOptions;
     setSelected(newSelected);
   };
 
+  const [pokemonData, setPokemonData] = useState<IPokemonDetail[]>([]);
+  const lastCalledRef = useRef<string[]>(['', '']);
+
   const onClickGo = async () => {
     console.log(selected);
+    const state = [...pokemonData];
+    if (lastCalledRef.current[0] !== selected[0].value) {
+      const data = await getPokemonDetail(selected[0].label);
+      if (data) state[0] = data;
+    }
+    if (lastCalledRef.current[1] !== selected[1].value) {
+      const data = await getPokemonDetail(selected[1].label);
+      if (data) state[1] = data;
+    }
+    setPokemonData(state);
+    lastCalledRef.current = [selected[0].value, selected[1].value];
   };
 
   return (
@@ -51,16 +68,10 @@ const Compare: React.FC = () => {
       <Typography variant="h3">Compare Pokémon</Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} md={5}>
-          <FormControl className={classes.select}>
-            <InputLabel id="demo-simple-select-label">Choose a Pokémon</InputLabel>
-            <Select options={pokemonList} value={selected[0]} onChange={handler(0)} className="capitalize" />
-          </FormControl>
+          <Select options={pokemonList} value={selected[0]} onChange={handler(0)} className={classes.select} />
         </Grid>
         <Grid item xs={12} md={5}>
-          <FormControl className={classes.select}>
-            <InputLabel id="demo-simple-select-label">Choose a Pokémon</InputLabel>
-            <Select options={pokemonList} value={selected[1]} onChange={handler(1)} className="capitalize" />
-          </FormControl>
+          <Select options={pokemonList} value={selected[1]} onChange={handler(1)} className={classes.select} />
         </Grid>
         <Grid item xs={2}>
           <Button variant="contained" color="primary" onClick={onClickGo}>
@@ -68,6 +79,16 @@ const Compare: React.FC = () => {
           </Button>
         </Grid>
       </Grid>
+      {pokemonData.length !== 0 && (
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <PokemonDetail pokemonDetail={pokemonData[0]} />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <PokemonDetail pokemonDetail={pokemonData[1]} />
+          </Grid>
+        </Grid>
+      )}
     </React.Fragment>
   );
 };
